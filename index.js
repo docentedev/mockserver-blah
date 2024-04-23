15a04b86afe00b1334d39be41b7847d7ba57a6cc
docente.dev <docente.dev@gmail.com>
2024-04-23 18:37:39 -0400

    feat: Mock Server 1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const propylsionTypesRoutes = require('./routes/commons')
+const commonsRoutes = require('./routes/commons')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(propylsionTypesRoutes);
+app.use(commonsRoutes);
 
 const port = 3009;
 
